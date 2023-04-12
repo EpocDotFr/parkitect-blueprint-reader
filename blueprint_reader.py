6c142c6bd358b58cfb5fa7f58412c4196d59695b
EpocDotFr <contact@epoc.fr>
2023-04-12 11:52:21 +0200

    Don't know if it's right

--- blueprint_reader.py
+++ blueprint_reader.py
@@ -1,23 +1,28 @@
 from typing import Dict, BinaryIO
+from bitstring import Bits
 from PIL import Image
 
 
 def read_blueprint(fp: BinaryIO) -> Dict:
     with Image.open(fp) as img:
         width, height = img.size
+        bands = len(img.getbands())
 
         pixels = img.load()
 
     fp.close()
 
-    raw_data = b''
+    bits = ''
 
     for x in range(width):
         for y in range(height):
             pixel = pixels[x, y]
 
-            for band in range(4):
-                raw_data += pixel[band] & 1
+            for band in range(bands):
+                bits += str(pixel[band] & 1)
+
+    with open('t.bin', 'wb') as fp:
+        Bits(bin='0b' + bits).tofile(fp)
 
     return {}
 
