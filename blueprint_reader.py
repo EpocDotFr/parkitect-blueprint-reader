4ee680f2bc0d4d6bb0c8e6c0961ee292449a8f8a
Maxime <EpocDotFr@users.noreply.github.com>
2023-05-03 11:20:33 +0200

    Stuff

--- blueprint_reader.py
+++ blueprint_reader.py
@@ -9,22 +9,22 @@ class BlueprintReadError(Exception):
     pass
 
 
-def _read_pixel(pixels: List, p: int) -> str:
+def read_pixel(pixels: List, p: int) -> str:
     return ''.join([
         str(pixels[p][band] & 1) for band in (3, 0, 1, 2) # Alpha, Red, Green, Blue
     ])
 
 
-def _read_pixels(pixels: List, start: int, stop: int) -> Bits:
+def read_pixels(pixels: List, start: int, stop: int) -> Bits:
     if start % 2 != 0 or stop % 2 != 0:
         raise ValueError('start and stop must be multiples of 2')
 
     return Bits(bin='0b' + ''.join([
-        _read_pixel(pixels, p + 1) + _read_pixel(pixels, p) for p in range(start, stop, 2)
+        read_pixel(pixels, p + 1) + read_pixel(pixels, p) for p in range(start, stop, 2)
     ]))
 
 
-def _decode(fp: BinaryIO) -> Bits:
+def read_blueprint(fp: BinaryIO) -> Dict:
     try:
         with Image.open(fp, formats=['png']) as img:
             if img.mode != 'RGBA':
@@ -41,31 +41,33 @@ def _decode(fp: BinaryIO) -> Bits:
     finally:
         fp.close()
 
-    # header = _read_pixels(pixels, 0, 6).tobytes()
+    # TODO Read and validate header
+    # header = read_pixels(pixels, 0, 6).tobytes()
 
     # if header != bytes.fromhex('53 4D 01'):
     #     raise BlueprintReadError('Not a Parkitect blueprint')
 
-    return _read_pixels(pixels, 0, 6)
+    data = read_pixels(pixels, 0, 6)
 
-    # gzip_size = _read_pixels(pixels, 6, 16).unpack('uint:16')[0]
+    # TODO Read and validate gzip data size
+    # gzip_size = read_pixels(pixels, 6, 16).unpack('uint:16')[0]
     #
     # if not gzip_size:
     #     raise BlueprintReadError('Could not determine gzip data size')
     # elif gzip_size > 512 * 512 / 2: # Number of pixels divided by 2 (1 byte is stored using 2 pixels)
     #     raise BlueprintReadError('Invalid gzip data size value')
-    #
-    # return _read_pixels(pixels, 16, gzip_size * 2)
 
+    # TODO Read and validate checksum
 
-def read_blueprint(fp: BinaryIO) -> Dict:
-    data = _decode(fp)
+    # TODO Read gzipped data, return parsed JSON data
+    # return json.loads(
+    #    gzip.decompress(
+    #        read_pixels(pixels, 16, gzip_size * 2).tobytes()
+    #    )
+    # )
 
     with open('actual.bin', 'wb') as fp:
         data.tofile(fp)
-
-    # with gzip.open(f, 'r') as fp:
-    #     data = json.loads(fp.read().decode('utf-8'))
 
     return {}
 
